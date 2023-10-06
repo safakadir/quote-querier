08b76bd4f259345d4824bf9c8028dbcb0102acba
safakadir <safakadir@gmail.com>
2023-10-06 14:12:11 +0300

    switched to express

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('YO!')
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log('Listening on port', port)
+})
